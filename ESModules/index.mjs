1441c39c5fc6db0cc56f7f7ba791f0471957ac0a
Fernanda Rios <ingfernandarios1214@gmail.com>
2023-09-24 20:04:29 -0500

    Sistemas de modulos

---
+++ ESModules/index.mjs
@@ -0,0 +1,3 @@
+import { sum, sub } from "./sum.mjs"; 
+console.log('Sum by ESModule', sum(5,7))
+console.log('SUB by ESModule', sub(5,7))
---
+++ cjs/index.js
@@ -0,0 +1,3 @@
+const {sum} = require('./sum')
+
+console.log('oooo', sum(8,9))
